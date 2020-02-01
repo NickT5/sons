e26380b3bd1c948510b9e6cf2d5647a095d40d1c
Nick Todts <nick-todts@hotmail.com>
2020-02-01 19:16:37 +0100

    Use omdb api also when seeding data.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Seeder;
 use \App\User;
 use \App\Movie;
 
+use \App\Http\Controllers\MovieController;
+
+
 class UsersTableSeeder extends Seeder
 {
     /**
@@ -12,31 +15,59 @@ class UsersTableSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
+    {        
         // Create a dummy user.
         $user = User::create([
-            'name' => 'Test',
-            'email' => 'test@hotmail.com',
-            'password' => Hash::make('testtest')
+            'name' => 'Nick',
+            'email' => 'nick@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('nick')
         ]);
 
-        // Create a few dummy movies that the user has (not) seen.
+        // Create a few dummy movies that the user has or hasn't seen.
+        $info = MovieController::call_omdb_api(['title'=>'John+Wick']);
         Movie::create([
             'title' => 'John Wick',
             'seen' => '1',
-            'user_id' => $user->id
+            'user_id' => $user->id,
+            'year' => $info['Year'],
+            'genre' => $info['Genre'],
+            'stars' => $info['Actors'],
+            'poster' => $info['Poster'],
+            'rating' => $info['imdbRating'],
+            'runtime' => $info['Runtime'],
+            'director' => $info['Director'],
+            'description' => $info['Plot']
         ]);
 
+        $info = MovieController::call_omdb_api(['title'=>'Knives+Out']);
         Movie::create([
             'title' => 'Knives Out',
             'seen' => '0',
-            'user_id' => $user->id
+            'user_id' => $user->id,
+            'year' => $info['Year'],
+            'genre' => $info['Genre'],
+            'stars' => $info['Actors'],
+            'poster' => $info['Poster'],
+            'rating' => $info['imdbRating'],
+            'runtime' => $info['Runtime'],
+            'director' => $info['Director'],
+            'description' => $info['Plot']
         ]);
 
+        $info = MovieController::call_omdb_api(['title'=>'1917']);
         Movie::create([
             'title' => '1917',
             'seen' => '0',
-            'user_id' => $user->id
+            'user_id' => $user->id,
+            'year' => $info['Year'],
+            'genre' => $info['Genre'],
+            'stars' => $info['Actors'],
+            'poster' => $info['Poster'],
+            'rating' => $info['imdbRating'],
+            'runtime' => $info['Runtime'],
+            'director' => $info['Director'],
+            'description' => $info['Plot']
         ]);
     }
 }
